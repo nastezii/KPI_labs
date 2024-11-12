2869c874d7b77e1510eb0488f7ec8182d8fc597d
Shalak Anastasia <nastiashalak@gmail.com>
2024-11-12 23:19:14 +0200

    the first task is completed

--- lab7/Program.cs
+++ lab7/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿//implementation of a function that removes an element from an array.
+void RemoveElement(ref int[] array, int item)
+{ 
+    List<int> list = array.ToList();
+    list.Remove(item);
+    int[] arr = list.ToArray();
+    array = arr;
+}
