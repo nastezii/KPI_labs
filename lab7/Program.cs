6c043f3319c062b6327e3a6566f615bf05217167
Shalak Anastasia <nastiashalak@gmail.com>
2024-11-12 23:34:12 +0200

    the second task is completed

--- lab7/Program.cs
+++ lab7/Program.cs
@@ -1,4 +1,4 @@
-﻿//implementation of a function that removes an element from an array.
+﻿//Implementation of a function that removes an element from an array.
 void RemoveElement(ref int[] array, int item)
 { 
     List<int> list = array.ToList();
@@ -6,3 +6,13 @@ void RemoveElement(ref int[] array, int item)
     int[] arr = list.ToArray();
     array = arr;
 }
+
+//Function, that removes the selected elements from the array.
+void RemoveElements(ref int[] array, params int[] items)
+{
+    List<int> list = array.ToList();
+    foreach (int item in items) { list.Remove(item); }
+    int[] arr = list.ToArray();
+    array = arr;
+}
+
