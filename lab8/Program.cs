da8b2716fae0f6d0851c0ed28572af0d32d03208
Shalak Anastasia <nastiashalak@gmail.com>
2024-12-18 13:22:06 +0200

    third task completed

--- lab8/Program.cs
+++ lab8/Program.cs
@@ -30,3 +30,30 @@ void Ex2()
         return () => value;
     }
 }
+
+//A function that iterates over the first argument and checks the argument types and the result type.
+
+void Ex3()
+{
+    var add = (int a, int b) => a + b;
+    var addNumbers = Contract(add);
+    Console.WriteLine(addNumbers(2, 3)); 
+
+    var concat = (string s1, string s2) => s1 + s2;
+    var concatStrings = Contract(concat);
+    Console.WriteLine(concatStrings("Hello ", "world!"));
+
+    Func<T1, T2, TResult> Contract<T1, T2, TResult>(Func<T1, T2, TResult> function)
+    {
+        return (arg1, arg2) =>
+        {
+            if (arg1 is not T1 || arg2 is not T2) { throw new ArgumentException("Invalid argument types."); }
+
+            TResult result = function(arg1, arg2);
+
+            if (result is not TResult) { throw new ArgumentException("Invalid result type.");  }
+
+            return result;
+        };
+     }
+}
