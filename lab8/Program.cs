837da70fb2cb7239147af39d47698720cc7c832f
Shalak Anastasia <nastiashalak@gmail.com>
2024-12-18 12:38:20 +0200

    first task completed

---
+++ lab8/Program.cs
@@ -0,0 +1,18 @@
+﻿//A function that iterates over a given object, passing each element to a callback with the following contract
+
+void Ex1()
+{ 
+    var pairs = new Dictionary<string, int> { { "a", 1 }, { "b", 2 }, { "c", 3 } };
+    Iterate(pairs, (key, value) =>
+    {
+        Console.WriteLine($"{{ key: '{key}', value: {value} }}");
+    });
+
+    void Iterate(Dictionary<string, int> pairs, Action<string, int> callback)
+    {
+        foreach (var pair in pairs)
+        {
+            callback(pair.Key, pair.Value);
+        }
+    }
+}
